3bf3c4bcdcf0d2ec8e8810bb0422173352fe33e2
Stephane Erbrech <stephane.erbrech@gmail.com>
2021-08-23 14:57:31 +1000

    use KVLogger to log within a span

--- opentracing/go.mod
+++ opentracing/go.mod
@@ -1,8 +1,11 @@
 module github.com/devigned/tab/opentracing
 
+go 1.16
+
 require (
 	github.com/devigned/tab v0.0.1
 	github.com/opentracing/opentracing-go v1.1.0
+	github.com/stretchr/testify v1.7.0
 )
 
 replace github.com/devigned/tab => ../.
---
+++ opentracing/opentracing_test.go
@@ -0,0 +1,23 @@
+package opentracing
+
+import (
+	"testing"
+
+	"github.com/opentracing/opentracing-go/mocktracer"
+	"github.com/stretchr/testify/assert"
+)
+
+
+
+func TestKVLogger_Info(t *testing.T) {
+	tracer := mocktracer.New()
+	span := tracer.StartSpan("test no error")
+	l := &KVLogger{Span: &Span{ span }}
+	l.Info("info")
+	l.Debug("debug")
+	span.Finish()
+
+	finishedSpan := tracer.FinishedSpans()[0]
+	logs := finishedSpan.Logs()
+	assert.Len(t, logs,2)
+}
